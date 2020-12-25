7fce10666e6c5f6661f57e2ec30c69a89ee762b4
Aya Omar <yotayot29@gmail.com>
2020-12-25 16:39:13 +0200

    api node

--- client/script.js
+++ client/script.js
@@ -8,3 +8,13 @@ fetch('http://localhost:3000/user',{
 .catch(err=>{
     console.log(err);
 })
+
+fetch('http://localhost:3000/user',{
+    method:'post',
+    headers: {
+        'Content-Type': 'application/json',
+      },
+}).then(res=>res.json()).then(val=>console.log(val))
+.catch(err=>{
+    console.log(err);
+})
